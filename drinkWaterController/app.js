ad1c2e4b3d40f8bc66e45f582b5c0ef38c16cbc1
italopussi <italopussi@gmail.com>
2020-11-01 13:45:56 -0300

    Drink Water Extras 1: Overflow and localStorage Preparation for stats

--- drinkWaterController/app.js
+++ drinkWaterController/app.js
@@ -1,19 +1,46 @@
-const box = document.querySelector('.box')
-const fill = document.querySelector(".fill")
+const tank = document.querySelector('.tank')
+const tankFill = document.querySelector(".tank .fill")
 const consumed = document.querySelector("#consumed")
-
-let totalConsumed = localStorage.getItem("waterQtd") == null ?  0 : parseInt(localStorage.getItem("waterQtd"))
+const lake = document.querySelector(".lake")
+let totalConsumed = localStorage.getItem("waterController") == null ?  0 : JSON.parse(localStorage.getItem("waterController"))[getTodayDateFormated()]
 function displayConsumed(){
 	consumed.textContent = `${totalConsumed} ML CONSUMED`
 }
+
 function fillWater(qtd){
-	fill.classList.add("now")
+	tankFill.classList.add("now")
 	totalConsumed+=qtd
+
 	let percentage = 100 - totalConsumed / 2000 * 100
 	percentage = percentage <0 ? 0 : percentage
-	fill.style.transform = `translateY(${percentage}%)`
+	tankFill.style.transform = `translateY(${percentage}%)`
 	displayConsumed()
-	localStorage.setItem("waterQtd", totalConsumed)
+
+	if(totalConsumed>2000){
+		let percentage = 100 - ((totalConsumed-2000) / 30)
+		percentage = percentage <0 ? 0 : percentage
+
+		lake.style.transform = `translateY(${percentage}%)`
+	}else{
+		lake.style.transform = `translateY(100%)`
+	}
+
+	handleSaveLocalStorage(totalConsumed)
+}
+
+function handleSaveLocalStorage(total){
+	const day = getTodayDateFormated()
+	let data 
+	if(localStorage.getItem("waterController") == null){
+		data = {[day]:total}
+	}else{
+		data = JSON.parse(localStorage.getItem("waterController"))
+		data = {
+			...data,
+			[day]: total
+		}
+	}
+	localStorage.setItem("waterController", JSON.stringify(data))
 }
 
 function reset(){
@@ -22,4 +49,11 @@ function reset(){
 }
 
 fillWater(0)
+
+// Return or today as string or as a date object
+function getTodayDateFormated(returnDataObject){
+	const dt = new Date()
+	const today = `${dt.getFullYear()}/${(dt.getMonth() + 1)}/${dt.getDate()}`
+	return returnDataObject ? new Date(today) : today
+}
 
