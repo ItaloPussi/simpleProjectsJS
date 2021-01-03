e1ce40a2ac4aaa4a49593905e028d7163caae9f5
ItaloPussi <italopussi@gmail.com>
2021-01-03 14:29:07 -0300

    Removed Portuguese text and beautify CSS/JS

--- listMe/app.js
+++ listMe/app.js
@@ -12,8 +12,8 @@ const onlyRadio = document.querySelector("#only")
 const typeSelect = document.querySelector("#task-type")
 const pickerDate = document.querySelector("#pickDate")
 
-const dynamicBox= document.querySelector("#dynamic")
-const dynamicOptions= document.querySelector(".dynamic-options")
+const dynamicBox = document.querySelector("#dynamic")
+const dynamicOptions = document.querySelector(".dynamic-options")
 const submitBtn = document.querySelector('.submit')
 const cancelBtn = document.querySelector('.cancel')
 const clearBtn = document.querySelector('.clear-btn')
@@ -25,7 +25,7 @@ let editElement;
 let editElementText;
 let editFlag = false;
 let editId = "";
-let currentId = localStorage.getItem('items') ? Number(localStorage.getItem('currentId'))+1 : 0;
+let currentId = localStorage.getItem('items') ? Number(localStorage.getItem('currentId')) + 1 : 0;
 let selectedType = ''
 let showAllTasks = false
 
@@ -37,7 +37,7 @@ resetBtn.addEventListener("click", resetItems)
 dynamicBox.addEventListener("change", handleDynamicOptionsDisplay)
 archiveBtn.addEventListener("click", handleDisplayAllTasks)
 
-typeSelect.addEventListener("change", (e)=>{
+typeSelect.addEventListener("change", (e) => {
 	selectedType = e.target.value
 	handleDisplayTasksWithSelectedType()
 })
@@ -45,9 +45,9 @@ typeSelect.addEventListener("change", (e)=>{
 pickerDate.min = pickerDate.value = getTodayDateFormated().replaceAll("/", "-")
 
 // Return or today as string or as a date object
-function getTodayDateFormated(returnDataObject){
+function getTodayDateFormated(returnDataObject) {
 	const dt = new Date()
-	
+
 	let month = dt.getMonth() + 1
 	month = month < 10 ? `0${month}` : month
 	let day = dt.getDate()
@@ -58,92 +58,96 @@ function getTodayDateFormated(returnDataObject){
 }
 
 // Verifiy if today is a weekday
-function todayIsWeekday(){
+function todayIsWeekday() {
 	const weekday = getTodayDateFormated(true).getDay()
 	return weekday == 0 || weekday == 6 ? false : true
 }
 
 //Date one is greater than date two?
-function compareTwoDates(date1, date2){
-	return date1>date2
+function compareTwoDates(date1, date2) {
+	return date1 > date2
 }
 
-function handleDisplayAllTasks(){
+function handleDisplayAllTasks() {
 	showAllTasks = !showAllTasks
-	typeSelect.disabled= showAllTasks ? true : false
+	typeSelect.disabled = showAllTasks ? true : false
 	archiveBtn.style.transform = showAllTasks ? "rotateZ(10deg)" : "rotateZ(0deg)"
 
-	if(showAllTasks){
+	if (showAllTasks) {
 		const items = [...list.children]
-		items.map(item=>{
-			item.style.display = ((item.dataset.type != selectedType) && selectedType != '') ? "none" : "flex" 
+		items.map(item => {
+			item.style.display = ((item.dataset.type != selectedType) && selectedType != '') ? "none" : "flex"
 		})
-	}
-	!showAllTasks && handleDisplayTasksWithSelectedType()
+	}!showAllTasks && handleDisplayTasksWithSelectedType()
 
 }
-function handleDisplayTasksWithSelectedType(){
+
+function handleDisplayTasksWithSelectedType() {
 	const items = [...list.children]
-	items.map(item=>{
-		if((item.dataset.type != selectedType) && selectedType != ''){
-			item.style.display= "none"
-		}else{
+	items.map(item => {
+		if ((item.dataset.type != selectedType) && selectedType != '') {
+			item.style.display = "none"
+		}
+		else {
 			const displayDay = new Date(item.dataset.displayday)
 			const frequencyDate = new Date(item.dataset.frequencydate)
-			if((!compareTwoDates(displayDay,getTodayDateFormated(true))) && (!compareTwoDates(frequencyDate,getTodayDateFormated(true)))){
-				if(item.dataset.frequency == 7 && !todayIsWeekday()){
+			if ((!compareTwoDates(displayDay, getTodayDateFormated(true))) && (!compareTwoDates(frequencyDate, getTodayDateFormated(true)))) {
+				if (item.dataset.frequency == 7 && !todayIsWeekday()) {
 					return item.style.display = "none"
-				}else{
+				}
+				else {
 					item.style.display = "flex"
 				}
-			}else{
+			}
+			else {
 				item.style.display = "none"
 			}
 		}
-	
+
 	})
 }
-function handleDynamicOptionsDisplay(){
+
+function handleDynamicOptionsDisplay() {
 	let toBeDisabled = dynamicBox.checked ? true : false
 	dynamicOptions.classList.toggle("hidden")
-	radioIpts.forEach(radio=>{
- 		radio.disabled = toBeDisabled
- 	})
+	radioIpts.forEach(radio => {
+		radio.disabled = toBeDisabled
+	})
 }
 
-function resetItems(){
+function resetItems() {
 	const items = [...list.children]
-	items.forEach(item=>{
+	items.forEach(item => {
 		const itemDataFrequency = item.getAttribute("data-frequency")
 		const itemIsCompleted = item.children[0].classList.value.includes("completed")
 		item.children[0].classList.remove("completed")
 
-		if(itemDataFrequency == 1 && itemIsCompleted){
+		if (itemDataFrequency == 1 && itemIsCompleted) {
 			list.removeChild(item)
 		}
-		if(itemDataFrequency > 2 && itemDataFrequency!=6){
+		if (itemDataFrequency > 2 && itemDataFrequency != 6) {
 			let day = getTodayDateFormated(true)
-			
-			if(itemDataFrequency == 3){
+
+			if (itemDataFrequency == 3) {
 				day.setDate(day.getDate() + 7)
 			}
-			if(itemDataFrequency == 4){
+			if (itemDataFrequency == 4) {
 				day.setDate(day.getDate() + 30)
 			}
 
-			if(itemIsCompleted){
+			if (itemIsCompleted) {
 				day = day.getFullYear() + "/" + (day.getMonth() + 1) + "/" + day.getDate()
 				item.setAttribute("data-frequencyDate", day)
 				item.style.display = "none"
 			}
 		}
-		if(itemDataFrequency == 6 && itemIsCompleted){
-			if(item.getAttribute("data-current") == item.getAttribute("data-max")){
+		if (itemDataFrequency == 6 && itemIsCompleted) {
+			if (item.getAttribute("data-current") == item.getAttribute("data-max")) {
 				list.removeChild(item)
 				return false
 			}
 			item.children[0].children[1].textContent = item.children[0].children[1].textContent.replace(item.getAttribute("data-current"), parseInt(item.getAttribute("data-current")) + 1)
-			item.setAttribute("data-current",parseInt(item.getAttribute("data-current")) + 1)
+			item.setAttribute("data-current", parseInt(item.getAttribute("data-current")) + 1)
 
 		}
 
@@ -152,7 +156,7 @@ function resetItems(){
 }
 
 
-function createAttb(element, attbName, attbValue){
+function createAttb(element, attbName, attbValue) {
 	const attr = document.createAttribute(`${attbName}`)
 	attr.value = attbValue
 	element.setAttributeNode(attr)
@@ -160,71 +164,72 @@ function createAttb(element, attbName, attbValue){
 }
 
 //Create a element by receiving a value and a valid ID
-function createElement(textValue, elementDataID,status, frequency, frequencyDate, dynamics, taskType, initialDisplayDay){
+function createElement(textValue, elementDataID, status, frequency, frequencyDate, dynamics, taskType, initialDisplayDay) {
 	let element = document.createElement('article')
-		element.classList.add('list-item')
+	element.classList.add('list-item')
 
-		element = createAttb(element, "data-id", elementDataID)
-		element = createAttb(element, "data-frequency", frequency)
-		element = createAttb(element, "data-type", taskType)
+	element = createAttb(element, "data-id", elementDataID)
+	element = createAttb(element, "data-frequency", frequency)
+	element = createAttb(element, "data-type", taskType)
 
 
-		if(!initialDisplayDay){
-			const date = new Date(pickerDate.value)
-			initialDisplayDay = compareTwoDates(getTodayDateFormated(true), date) ? getTodayDateFormated(false) : pickerDate.value
+	if (!initialDisplayDay) {
+		const date = new Date(pickerDate.value)
+		initialDisplayDay = compareTwoDates(getTodayDateFormated(true), date) ? getTodayDateFormated(false) : pickerDate.value
+	}
+
+	if (initialDisplayDay) {
+		const date = new Date(initialDisplayDay)
+		if (compareTwoDates(date, getTodayDateFormated(true))) {
+			element.style.display = "none"
 		}
 
-		if(initialDisplayDay){
-			const date = new Date(initialDisplayDay)
-			if(compareTwoDates(date,getTodayDateFormated(true))){
-				element.style.display = "none"
-			}
+		element = createAttb(element, "data-displayDay", initialDisplayDay)
+	}
 
-			element = createAttb(element, "data-displayDay", initialDisplayDay)
+
+	if (frequency == 7) {
+		const today = getTodayDateFormated(true)
+		weekday = parseInt(today.getDay())
+
+		if (!todayIsWeekday()) {
+			element.style.display = "none"
 		}
+	}
+	if (frequency > 2 && frequency != 6 && frequency != 7) {
+		frequencyDayValue = frequencyDate ? frequencyDate : getTodayDateFormated(false)
 
+		element = createAttb(element, "data-frequencyDate", frequencyDayValue)
 
-		if(frequency ==7 ){
+		if (frequencyDate) {
 			const today = getTodayDateFormated(true)
-			weekday = parseInt(today.getDay())
+			frequencyDate = new Date(frequencyDate)
 
-			if(!todayIsWeekday()){
+			if (compareTwoDates(frequencyDate, today)) {
 				element.style.display = "none"
 			}
 		}
-		if(frequency > 2 && frequency != 6 && frequency != 7){
-			frequencyDayValue = frequencyDate ? frequencyDate : getTodayDateFormated(false)  
+	}
 
-			element = createAttb(element, "data-frequencyDate", frequencyDayValue)
+	if (frequency == 6 && !dynamics) {
+		const current = document.querySelector("input[name=current]").value
+		const max = document.querySelector("input[name=max]").value
 
-			if(frequencyDate){
-				const today =  getTodayDateFormated(true)
-				frequencyDate = new Date(frequencyDate)
+		textValue = textValue.replace("$", current)
+		textValue += ` (${max})`
 
-				if(compareTwoDates(frequencyDate, today)){
-					element.style.display = "none"
-				}
-			}
-		}
-
-		if(frequency == 6 && !dynamics){
-			const current = document.querySelector("input[name=current]").value
-			const max = document.querySelector("input[name=max]").value
-				
-			textValue = textValue.replace("$", current)
-			textValue+=` (${max})`
-
-			element = createAttb(element, "data-current", current)
-			element = createAttb(element, "data-max", max)
+		element = createAttb(element, "data-current", current)
+		element = createAttb(element, "data-max", max)
 
 
-		}else if(frequency == 6 && dynamics){
+	}
+	else if (frequency == 6 && dynamics) {
 
-			element = createAttb(element, "data-current", dynamics.currentValue)
-			element = createAttb(element, "data-max", dynamics.maxValue)
-		}
+		element = createAttb(element, "data-current", dynamics.currentValue)
+		element = createAttb(element, "data-max", dynamics.maxValue)
+	}
 
-		element.innerHTML = `
+	element.innerHTML = `
 			<p class="title ${status ? 'completed' : ''}" onClick=changeStatus(${elementDataID})>
 				<span class="type" data-type="${taskType}" ></span>
 				<span class="text">${textValue}</span>
@@ -239,76 +244,78 @@ function createElement(textValue, elementDataID,status, frequency, frequencyDate
 				</button>
 			</div>
 		`
-		list.appendChild(element)
-		container.classList.add('show-container')
+	list.appendChild(element)
+	container.classList.add('show-container')
 
 }
 
 //Handle the submit action triggered by the user
-function addItem(e){
+function addItem(e) {
 	e.preventDefault()
 
 	const selectedFrequency = dynamicBox.checked ? 6 : document.querySelector('input[type = radio]:checked').value
 
-	if(selectedType === ""){
+	if (selectedType === "") {
 		displayAlert("Please select a valid type", "danger")
 		return false;
 	}
-	
+
 	const valueIpt = itemIpt.value
 
-	if (valueIpt && !editFlag){
-		
+	if (valueIpt && !editFlag) {
 
-		createElement(valueIpt, currentId, false,selectedFrequency, false, false, parseInt(selectedType), false)
+
+		createElement(valueIpt, currentId, false, selectedFrequency, false, false, parseInt(selectedType), false)
 		displayAlert('Item add with success!', 'success')
 		resetParams()
 		localStorage.setItem('currentId', currentId)
 		currentId++
 
-	}else if(valueIpt && editFlag){
+	}
+	else if (valueIpt && editFlag) {
 		editElement.setAttribute("data-type", parseInt(selectedType))
 		editElement.querySelector(".type").setAttribute("data-type", parseInt(selectedType))
 		editElementText.textContent = valueIpt
 		displayAlert('Item edited with success!', 'success')
 		resetParams()
-	}else{
+	}
+	else {
 		displayAlert('Please enter value', 'danger')
 	}
 	handleLocalStorage()
 }
 
 //Change the completed status by click
-function changeStatus(clickedId){
+function changeStatus(clickedId) {
 	const clickedItem = document.querySelector(`[data-id="${clickedId}"]`).children[0]
-	clickedItem.classList.length>1 ? clickedItem.classList.remove("completed") : clickedItem.classList.add("completed")
+	clickedItem.classList.length > 1 ? clickedItem.classList.remove("completed") : clickedItem.classList.add("completed")
 	handleLocalStorage()
 }
 
 //Edit the text of a item
-function editItem(targetId){
- 	editFlag=true
- 	editId=targetId
+function editItem(targetId) {
+	editFlag = true
+	editId = targetId
 	editElement = document.querySelector(`[data-id="${targetId}"]`)
 	editElementText = editElement.querySelector(".text")
-	 console.log(editElement)
- 	submitBtn.textContent = 'edit'
- 	itemIpt.value = editElementText.textContent.trim()
- 	itemIpt.focus()
- 	cancelBtn.classList.remove('hidden')
-	 dynamicBox.disabled = true
-	 typeSelect.value = editElement.getAttribute("data-type")
-	 selectedType = editElement.getAttribute("data-type")
- 	radioIpts.forEach(radio=>{
- 		radio.disabled = true
- 	})
+	console.log(editElement)
+	submitBtn.textContent = 'edit'
+	itemIpt.value = editElementText.textContent.trim()
+	itemIpt.focus()
+	cancelBtn.classList.remove('hidden')
+	dynamicBox.disabled = true
+	typeSelect.value = editElement.getAttribute("data-type")
+	selectedType = editElement.getAttribute("data-type")
+	radioIpts.forEach(radio => {
+		radio.disabled = true
+	})
 }
 
 //Delete determined item
-async function deleteItem(targetId){
+async function deleteItem(targetId) {
 	const element = document.querySelector(`[data-id="${targetId}"]`)
 	list.removeChild(element)
-	if(list.children.length == 0){
+	if (list.children.length == 0) {
 		container.classList.remove('show-container')
 		localStorage.removeItem("items")
 		localStorage.removeItem("currentId")
@@ -319,7 +326,7 @@ async function deleteItem(targetId){
 }
 
 //Delete all items
- function clearAllItems(){
+function clearAllItems() {
 	let confirm = window.prompt("Digite CONFIRMAR para apagar TUDO")
 
 	if (confirm != "CONFIRMAR") return
@@ -329,23 +336,23 @@ async function deleteItem(targetId){
 	localStorage.removeItem("currentId")
 	resetParams()
 	displayAlert('Removed all items', 'success')
-	currentId=1
+	currentId = 1
 }
 
 //Handle alert interaction with the user
-function displayAlert(text, action){
+function displayAlert(text, action) {
 	alert.textContent = text
 	alert.classList.add(`alert-${action}`)
-	setTimeout(function(){
+	setTimeout(function () {
 		alert.classList.remove(`alert-${action}`)
-		alert.textContent=''
-	},1500)
+		alert.textContent = ''
+	}, 1500)
 
 }
 
 //Reset specific params
-function resetParams(){
-	itemIpt.value=""
+function resetParams() {
+	itemIpt.value = ""
 	editFlag = false;
 	editId = ""
 	submitBtn.textContent = "submit"
@@ -354,30 +361,30 @@ function resetParams(){
 	dynamicBox.checked = false;
 	dynamicOptions.classList.add("hidden")
 	dynamicBox.disabled = false
-	radioIpts.forEach(radio=>{
- 		radio.disabled = false
- 	})
-	 typeSelect.value = ''
-	 typeSelect.disabled = false
- 	selectedType = ''
- 	handleDisplayTasksWithSelectedType()
+	radioIpts.forEach(radio => {
+		radio.disabled = false
+	})
+	typeSelect.value = ''
+	typeSelect.disabled = false
+	selectedType = ''
+	handleDisplayTasksWithSelectedType()
 }
 
 //Control the data input on LocalStorage
-function handleLocalStorage(){
+function handleLocalStorage() {
 	const items = [...list.children]
-	const itemsContent = items.map(item =>{
+	const itemsContent = items.map(item => {
 		dynamics = false;
-		if(item.getAttribute("data-frequency")==6){
+		if (item.getAttribute("data-frequency") == 6) {
 			dynamics = {
 				"currentValue": item.getAttribute("data-current"),
 				"maxValue": item.getAttribute("data-max")
 			}
 		}
 		return {
-			"id":item.getAttribute('data-id'),
-			"value": item.children[0].textContent.replaceAll("\n","").replaceAll("\t", ""),
-			"completed": item.children[0].classList.length>1 ? true : false,
+			"id": item.getAttribute('data-id'),
+			"value": item.children[0].textContent.replaceAll("\n", "").replaceAll("\t", ""),
+			"completed": item.children[0].classList.length > 1 ? true : false,
 			"frequency": item.getAttribute("data-frequency"),
 			"frequencyDate": item.getAttribute("data-frequencyDate") ? item.getAttribute("data-frequencyDate") : false,
 			dynamics,
@@ -385,16 +392,16 @@ function handleLocalStorage(){
 			"initialDisplayDay": item.getAttribute("data-displayDay"),
 		}
 	})
-	localStorage.setItem('items',JSON.stringify(itemsContent))
+	localStorage.setItem('items', JSON.stringify(itemsContent))
 }
 
 //Retrieve the data from localStorage
-function recoverLocalStorageData(){
+function recoverLocalStorageData() {
 	const items = JSON.parse(localStorage.getItem('items'))
-	if(!items) return
+	if (!items) return
 
-	items.map(item=>{
-		createElement(item.value,item.id,item.completed, item.frequency, item.frequencyDate, item.dynamics, item.type, item.initialDisplayDay)
+	items.map(item => {
+		createElement(item.value, item.id, item.completed, item.frequency, item.frequencyDate, item.dynamics, item.type, item.initialDisplayDay)
 	})
 }
 
